0f5ed9adf7ae98d11108f24479c5b5909ba2cf42
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2024-10-13 16:18:21 +0600

    update1

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,14 @@ import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
+import featureImg from "../assets/images/feature-img.png";
+import faqImg from "../assets/images/faq-img.png";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import About from "../components/About/About";
 import ServicesList from "../components/Services/ServicesList";
+import DoctorList from "../components/Doctors/DoctorList";
+import FaqList from "../components/faq/faqList";
 
 const Home = () => {
   return (
@@ -184,6 +188,80 @@ const Home = () => {
         </div>
       </section>
       {/* services section end here */}
+
+      {/* feature section start here */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* feature content */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get virtual treatment <br /> anytime.
+              </h2>
+              <ul className="pl-4">
+                <li className="text__para">
+                  1. Schedule the appointment directly.
+                </li>
+                <li className="text__para">
+                  2. Search for your physician here, and contact their office.
+                </li>
+                <li className="text__para">
+                  3. Wait for your appointment to be confirmed.
+                </li>
+              </ul>
+
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+
+            {/* feature img */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} alt="feature img" className="w-3/4" />
+            </div>
+
+            {/* feature content */}
+          </div>
+        </div>
+      </section>
+
+      {/* feature section end here */}
+
+      {/* our great doctors */}
+      <section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">Our Great Doctors</h2>
+            <p className="text__para text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+              incidunt itaque vitae voluptate qui hic, ipsum saepe doloremque
+              quisquam aut.
+            </p>
+          </div>
+          {/* import doctor list */}
+          <DoctorList />
+          {/* end import doctor list */}
+        </div>
+      </section>
+      {/* out great doctors end */}
+
+      {/* faq section code start here */}
+      <section>
+        <div className="container">
+          <div className="flex justify-between gap-[50px] lg:gap-0">
+            <div className="w-1/2 hidden md:block">
+              <img src={faqImg} alt="faq-img" />
+            </div>
+            <div className="w-full md:w-1/2">
+              <h2 className="heading">
+                Most questions by our beloved patients
+              </h2>
+              <FaqList />
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* faq section end here */}
     </>
   );
 };
