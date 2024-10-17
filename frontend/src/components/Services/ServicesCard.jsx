4af853be0fa2d30b544ec9c9692bbaf87be68098
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2024-10-17 22:39:13 +0600

    updated

--- frontend/src/components/Services/ServicesCard.jsx
+++ frontend/src/components/Services/ServicesCard.jsx
@@ -21,7 +21,7 @@ const ServicesCard = ({ item, index }) => {
             <BsArrowRight className="group-hover:text-white w-6 h-5" />
           </Link>
           <span
-            className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]"
+            className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600] serviceBtn"
             style={{
               background: `{bgColor}`,
               color: `{textColor}`,
